ed0bef0d93fe9ef402e16ad5dd1b4d68aa084b41
Eswar Oruganti <eswaroruganti@Mac.lan>
2025-09-23 17:55:35 +0530

    fix: added names to form tags

--- src/components/Contact/index.jsx
+++ src/components/Contact/index.jsx
@@ -1,41 +1,46 @@
 import styles from "./Contact.module.css";
+import { useState } from "react";
 
 export default function Contact() {
+  const [submitted, setSubmitted] = useState(false);
+
   const handleSubmit = (event) => {
     event.preventDefault();
-
-    const myForm = event.target;
-    const formData = new FormData(myForm);
+    const form = event.target;
+    const formData = new FormData(form);
 
     fetch("/", {
       method: "POST",
       headers: { "Content-Type": "application/x-www-form-urlencoded" },
       body: new URLSearchParams(formData).toString(),
     })
-      .then(() => alert("Thank you for your submission"))
+      .then(() => setSubmitted(true))
       .catch((error) => alert(error));
   };
 
+  if (submitted) {
+    return <p>Thank you for your message! I’ll get back to you soon ✅</p>;
+  }
+
   return (
-    <>
-      <form
-        name="contact"
-        method="POST"
-        data-netlify="true"
-        onSubmit={handleSubmit}
-        className={styles.contactForm}
-      >
-        <input type="hidden" name="form-name" value="contact" />
-        <input placeholder="Name" required />
-        <input placeholder="Email" type="email" required />
-        <textarea
-          placeholder="Message"
-          rows={7}
-          maxlength="750"
-          required
-        ></textarea>
-        <button>Send Message</button>
-      </form>
-    </>
+    <form
+      name="contact"
+      method="POST"
+      data-netlify="true"
+      onSubmit={handleSubmit}
+      className={styles.contactForm}
+    >
+      <input type="hidden" name="form-name" value="contact" />
+      <input name="name" placeholder="Name" required />
+      <input name="email" placeholder="Email" type="email" required />
+      <textarea
+        name="message"
+        placeholder="Message"
+        rows={7}
+        maxLength={750}
+        required
+      />
+      <button type="submit">Send Message</button>
+    </form>
   );
 }
